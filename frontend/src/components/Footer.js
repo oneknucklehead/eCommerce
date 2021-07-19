c3384f3cbaaf117e9ae958a4e11f78ca6403f133
Zoheb Ahmed <zohebcool1542@gmail.com>
2021-07-19 23:46:40 +0530

    basic UI

---
+++ frontend/src/components/Footer.js
@@ -0,0 +1,19 @@
+import React from 'react'
+import { Row, Col } from 'react-bootstrap'
+import { Container } from 'react-bootstrap'
+
+const Footer = () => {
+  return (
+    <>
+      <footer>
+        <Container>
+          <Row>
+            <Col className='text-center py-3'>Copyright &copy; Smol.</Col>
+          </Row>
+        </Container>
+      </footer>
+    </>
+  )
+}
+
+export default Footer
